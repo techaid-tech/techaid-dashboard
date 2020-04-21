6c59636aa09d61b1522457aac09fe1143053afc2
Steve Akuom <steve@ju.ma>
2020-04-21 15:22:25 +0100

    Fixing map margin

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,1 +1,1 @@
-export const APP_VERSION = {version: '1.0.0', build: '20.04.21-1213', date: '2020-04-21T11:13:58.916Z'};
+export const APP_VERSION = {version: '1.0.0', build: '20.04.21-1517', date: '2020-04-21T14:17:29.642Z'};
