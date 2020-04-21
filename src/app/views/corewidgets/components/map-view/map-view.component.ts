9ae5710437b92979d9028fcc0f3d65452088a141
Steve Akuom <steve@ju.ma>
2020-04-21 15:31:21 +0100

    Adding icon for other

--- src/app/views/corewidgets/components/map-view/map-view.component.ts
+++ src/app/views/corewidgets/components/map-view/map-view.component.ts
@@ -155,7 +155,8 @@ export class MapViewComponent {
         'LAPTOP': 'https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-laptop&size=50&hoffset=0&voffset=-1&background=30475E',
         'TABLET': 'https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-tablet&size=50&hoffset=0&voffset=-1&background=D8345F',
         'SMARTPHONE': 'https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-mobile&size=50&hoffset=0&voffset=-1&background=DE7118',
-        'ALLINONE': 'https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-tv&size=50&hoffset=0&voffset=-1&background=9C5517'
+        'ALLINONE': 'https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-tv&size=50&hoffset=0&voffset=-1&background=9C5517',
+        'OTHER': 'https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-network-wired&size=50&hoffset=0&voffset=-1&background=163F5F'
       };
       res.data['kits'].forEach(k => {
         if(k.coordinates && k.coordinates.lat){
