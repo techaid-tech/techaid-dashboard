9f6ea1fd4a85d69961a81bf86f1287fd5cc17ec0
Steve Akuom <steve@ju.ma>
2020-05-11 15:44:40 +0100

    Fixing order

--- src/app/views/corewidgets/components/donor-index/donor-index.component.ts
+++ src/app/views/corewidgets/components/donor-index/donor-index.component.ts
@@ -198,7 +198,7 @@ export class DonorIndexComponent {
         "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
       pageLength: 10,
       lengthMenu: [ 5, 10, 25, 50, 100 ],
-      order: [1, 'desc'],
+      order: [3, 'desc'],
       serverSide: true,
       stateSave: true,
       processing: true,
