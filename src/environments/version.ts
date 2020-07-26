d470917a773af5c699e94df26e134b6507886dbd
Steve Akuom <steve@ju.ma>
2020-07-26 17:18:31 +0100

    Updates to nginx config

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,1 +1,1 @@
-export const APP_VERSION = {version: '1.0.0', build: '20.07.26-1704', date: '2020-07-26T16:04:54.679Z'};
+export const APP_VERSION = {version: '1.0.0', build: '20.07.26-1712', date: '2020-07-26T16:12:16.387Z'};
