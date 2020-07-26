7f70be10753319d97e28d9a9efb30ba4d85094df
Steve Akuom <steve@ju.ma>
2020-07-26 17:12:12 +0100

    Removing redirects

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,1 +1,1 @@
-export const APP_VERSION = {version: '1.0.0', build: '20.07.26-1628', date: '2020-07-26T15:28:24.480Z'};
+export const APP_VERSION = {version: '1.0.0', build: '20.07.26-1704', date: '2020-07-26T16:04:54.679Z'};
