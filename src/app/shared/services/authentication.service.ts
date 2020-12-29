16e4f9d0a900399c15d77bc29cef048e585abb3c
Steve Akuom <steve@ju.ma>
2020-12-29 11:49:04 +0000

    Fixing auth url

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -87,7 +87,7 @@ export class AuthenticationService {
     this.auth0Client$.subscribe((client: Auth0Client) => {
       // Call method to log in
       client.loginWithRedirect({
-        redirect_uri: `${window.location.origin}/app/`,
+        redirect_uri: `${window.location.origin}`,
         appState: { target: redirectPath }
       });
     });
