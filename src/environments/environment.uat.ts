232107c64f0550b83a8bb4ecb6f0a0aa2a062650
Tony Anzelmo <tony.anzelmo@gmail.com>
2023-08-22 15:50:19 +0100

    Add UAT environment, and parameterize env in the angular build

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,19 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+declare var require: any;
+import { APP_VERSION } from './version';
+
+import { ConfigParams } from '@app/state/config-params';
+
+export const environment: ConfigParams = {
+  production: false,
+  environment: 'uat',
+  graphql_endpoint: 'https://api.testing.communitytechaid.org.uk/graphql',
+  version: APP_VERSION,
+  auth_endpoint: 'https://api.testing.communitytechaid.org.uk/auth/user',
+  auth_audience: 'https://api.communitytechaid.org.uk',
+  auth_enabled: true,
+  remote_config: false,
+};
