8076acbfa9d60bd73c6eec1a02674a227747f862
Tony Anzelmo <tony.anzelmo@gmail.com>
2023-08-24 11:34:45 +0100

    Remove timeago formatting

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { InputMaskComponent } from './components/input-mask/input-mask.component';
 import { AuthGuard } from './services/auth.guard';
 import { ConfigService } from './services/config.service';
-import { TimeAgoPipe } from './pipes/timeago.pipe';
 import { YesNoPipe } from './pipes/yesno.pipe';
 import { AppInitialComponent } from './components/app-initial/app-initial.component';
 import { AuthenticationService } from './services/authentication.service';
@@ -14,13 +13,11 @@ import { CsvService } from './services/csv.service';
     ],
     exports: [
         InputMaskComponent,
-        TimeAgoPipe,
         YesNoPipe,
         AppInitialComponent
     ],
     declarations: [
         InputMaskComponent,
-        TimeAgoPipe,
         YesNoPipe,
         AppInitialComponent,
     ]
